98caccaf0d75198bf9c32c9790a857cec110a611
Dexa92 <rklegends92@gmail.com>
2015-10-23 12:16:05 +0300

    heroku rake db:migrate

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -5,7 +5,7 @@ class RegionsController < ApplicationController
   end
 
   def index
-    @regions = Region.all
+    @regions = Region.paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
